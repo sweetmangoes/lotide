aecdbf72b8dba5ea720b25f9bde0d470f2f2cb3f
sweetmangoes <alphonsechristian@gmail.com>
2022-09-25 13:59:13 -0400

    adding template string

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('🤑 Assertion Passed: ', actual,' === ', expected);
+    console.log(`🤑 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.assert(actual === expected, actual,' !== ', expected);
+    console.assert(actual === expected, `${actual} !== ${expected}`);
   }
-};
+  }
 
 assertEqual('Lighthouse Labs', 'Bootcamp');
 assertEqual(1,1);
